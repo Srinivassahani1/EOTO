eaaaa78ce19dc281ad453f5fc44f3d2edbe6a312
Srinivassahani1 <93371755+Srinivassahani1@users.noreply.github.com>
2021-10-29 13:07:10 +0530

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,9 @@
+function redirect(){
+    window.location.href = "./contact.html";
+}
+function function1(){
+    window.location.href = "./form1.html";
+}
+function function2(){
+    window.location.href = "./form2.html";
+}
